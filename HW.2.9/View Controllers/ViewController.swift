f905160ff2e64cf9e23dc9708241b0d3cce1ebef
ageres7-dev <66694244+ageres7-dev@users.noreply.github.com>
2020-11-11 02:22:37 +0500

    screen

--- HW.2.9/View Controllers/ViewController.swift
+++ HW.2.9/View Controllers/ViewController.swift
@@ -6,16 +6,42 @@
 //
 
 import UIKit
+import Spring
 
 class SpringViewController: UIViewController {
     
+    @IBOutlet var imageSpring: SpringImageView!
     
-
+    @IBOutlet var infoSpringLabel: SpringLabel!
+    
+    
+    @IBOutlet var infoText: SpringView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        infoSpringLabel.transform = CGAffineTransform(rotationAngle: 0.14)
     }
 
-
+    
+    
+    
+    @IBAction func buttonPressed() {
+        
+        
+        infoText.animation = "flipX"
+    
+        
+        
+        infoText.animate()
+        
+        
+        imageSpring.animation = "flipX"
+        imageSpring.curve = "easeIn"
+        imageSpring.duration = 1
+        imageSpring.animate()
+    }
+    
+    
 }
 
